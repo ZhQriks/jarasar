52ebfec7517c1d3f9e53278170acddc245a85c9a
Jarasar <zhorikmartanov228@gmail.com>
2026-01-12 09:29:47 -0600

    feat: analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -43,6 +44,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" style={{}}>
+      <Analytics />
       <body
         className={inter.className}
         style={{
